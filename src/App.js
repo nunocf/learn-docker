80179d5728273115c6b8ab3dce0689048a52d6cc
Nuno Ferreira <nunogcferreira@gmail.com>
2018-09-18 20:51:23 +0100

    added travis deployment config

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
+                    <h1 className="App-title">Welcome to my awesome webpage, Oli!</h1>
                 </header>
-                <p className="App-intro">I was changed!</p>
+                <p className="App-intro">Is it VERY NAICE?</p>
             </div>
         );
     }
